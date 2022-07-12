308eb7bd7183700e93d426d5feef18ce8cc1bff5
SvyatoslavBugrov <00svyatoslav00@mail.ru>
2022-07-12 16:24:09 +0300

    testing...

--- source/IDmodule.py
+++ source/IDmodule.py
@@ -16,22 +16,22 @@ class IDGen:
     def __init__(self):
         """ Constructor """
         self.c = 1
-        self.t = self.epoch()
+        self.t = self.__epoch()
         self.mac = str(get_mac() & 0x000000000FFF)
 
-    def get_c(self):
+    def __get_c(self):
         """ generates extra number for unification.
         should not be called by the user """
-        t2 = self.epoch()
+        t2 = self.__epoch()
         if (int(t2) - int(self.t)) > 1000 or self.c > 100:  # resets counter if millisecond has passed or c > 100
             self.t = t2
             self.c = 1
-        temp = self.digits(self.c)[1:] + str(self.c)
+        temp = self.__digits(self.c)[1:] + str(self.c)
         self.c += 1
         return temp
 
     @staticmethod
-    def digits(n):
+    def __digits(n):
         """ returns x zeros, where x is 4 minus the amount of digits in a number.
          should not be called by the user """
         b = 0
@@ -41,15 +41,18 @@ class IDGen:
             b = 1
         return "0" * (4 - b)
 
-    def epoch(self):
+    def __epoch(self):
         """returns a string which is a time of a call written without separators.
          should not be called by the user """
         d = datetime.now()
         y = d.year - 2020  # custom epoch
-        z = self.digits(y)
+        z = self.__digits(y)
         return z + str(y)+str(d.month)+str(d.day)+str(d.hour)+str(d.minute)+str(d.second)+str(d.microsecond)
 
     def get_id(self):
         """returns a new unique ID. should be called by the user"""
-        return int("0" + self.epoch() + str(self.mac) + self.get_c())
+        return int("0" + self.__epoch() + str(self.mac) + self.__get_c())
 
+
+a = IDGen()
+print(a.get_id())
--- tests/test__IDGen.py
+++ tests/test__IDGen.py
@@ -4,34 +4,35 @@ from source.IDmodule import IDGen
 
 class IDGenTest(unittest.TestCase):
     def test_digits(self):
-        self.assertEqual(IDGen.digits(321), '0')
-        self.assertEqual(IDGen.digits(4), '000')
-        self.assertEqual(IDGen.digits(0), '000')
-        self.assertEqual(IDGen.digits(32), '00')
+        t = IDGen()
+        self.assertEqual(t.__gigits(321), '0')
+        self.assertEqual(IDGen.__digits(4), '000')
+        self.assertEqual(IDGen.__digits(0), '000')
+        self.assertEqual(IDGen.__digits(32), '00')
 
     def test_get_c(self):
         a = IDGen()
         for i in range(30):
-            a.get_c()
-        self.assertEqual(a.get_c(), '031')
+            a.__get_c()
+        self.assertEqual(a.__get_c(), '031')
 
         a = IDGen()
         for i in range(64):
-            a.get_c()
-        self.assertEqual(a.get_c(), '065')
+            a.__get_c()
+        self.assertEqual(a.__get_c(), '065')
 
         a = IDGen()
         for i in range(99):
-            a.get_c()
-        self.assertEqual(a.get_c(), '100')
+            a.__get_c()
+        self.assertEqual(a.__get_c(), '100')
 
         a = IDGen()
         for i in range(100):
-            a.get_c()
-        self.assertEqual(a.get_c(), '001')
+            a.__get_c()
+        self.assertEqual(a.__get_c(), '001')
 
         a = IDGen()
-        self.assertEqual(a.get_c(), '001')
+        self.assertEqual(a.__get_c(), '001')
 
 
 if __name__ == '__main__':
